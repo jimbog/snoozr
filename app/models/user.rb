5dc85c6ad8c7a2aeeb60b474dfc5f3208a1fd338
Mia VanValkenburg <mia.vanvalkenburg@gmail.com>
2015-01-08 18:54:46 -0800

    added validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,39 @@
 class User
   include Mongoid::Document
+  field :name, type: String
   field :username, type: String
   field :email, type: String
-
+  field :password_digest, type: String
+  attr_reader :password
   has_many :posts
+
+  # from carrierwave
+  # mount_uploader :image, AvatarUploader #this field will be an image, carrierwave class for Avatars
+
+  #setter method
+  def password=(unencrypted_password)
+    unless unencrypted_password.empty?
+      @password = unencrypted_password
+      self.password_digest = BCrypt::Password.create(unencrypted_password)
+    end
+  end
+
+  # getter method for password - this is the method that get written by the "attr_reader :password" above
+  # def password
+  #   @password
+  # end
+
+
+  def authenticate(unencrypted_password)
+    if BCrypt::Password.new(self.password_digest) == unencrypted_password
+      return self
+    else
+      return false
+    end
+  end
+
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: { case_sensitive: false }, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i }
+  validates :password, presence: true, length: { in: 6..20 }, confirmation: true
+
 end
