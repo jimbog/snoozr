3c17c90c9f15c9b16ff9d38e86dbd6362e1b07e5
Mia VanValkenburg <mia.vanvalkenburg@gmail.com>
2015-01-12 15:02:27 -0800

    fixed login form

--- Gemfile
+++ Gemfile
@@ -44,6 +44,7 @@ gem 'jbuilder', '~> 2.0'
 gem 'sdoc', '~> 0.4.0', group: :doc
 
 # Use ActiveModel has_secure_password
+gem 'quiet_assets'
 
 
 
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,7 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(email: params[:login][:email])
+    user = User.where(email: params[:login][:email]).first
     if user && user.authenticate(params[:login][:password])
       session[:user_id] = user.id.to_s
       redirect_to user_path(current_user)
