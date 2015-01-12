51b97d14e4443d622939848089b1e9c9885628a6
Mia VanValkenburg <mia.vanvalkenburg@gmail.com>
2015-01-12 12:06:10 -0800

    fixed user relationship

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -5,10 +5,6 @@ class Comment
 
   embedded_in :post
 
-  #THIS IS WHERE THE ERROR IS
-  # belongs_to :user
-  accepts_nested_attributes_for :user
-
   # f.fields_for in show.html
   # update params in comments_controller
   def date_added
