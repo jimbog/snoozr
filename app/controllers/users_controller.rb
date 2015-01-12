0a6f49a223427dd55c5b9dab470077610b1c093e
Mia VanValkenburg <mia.vanvalkenburg@gmail.com>
2015-01-12 12:29:03 -0800

    fixed typo in users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,7 +13,7 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-    if @uesr.save
+    if @user.save
       session[:user_id] = @user.id.to_s
       redirect_to user_path(current_user)
     else
